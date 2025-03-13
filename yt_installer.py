2813aef645c738fed43fa7ada03cca5548426b92
Diego Caracas Mendes <diegocaracasmendes02@gmail.com>
2025-03-13 18:55:22 -0300

    Add Relative Paths and .exe

--- yt_installer.py
+++ yt_installer.py
@@ -4,19 +4,17 @@ import subprocess
 
 def download_video(youtube_url, download_path):
     try:
-        # Certifique-se de que o diretório existe
         if not os.path.exists(download_path):
             os.makedirs(download_path)
 
-        # Comando para baixar o vídeo com yt-dlp
         command = [
             "yt-dlp",
+            "--merge-output-format", "mp4",  # Garante saída final como MP4
             youtube_url,
             "-o", os.path.join(download_path, "%(title)s.%(ext)s"),
             "-f", "bestvideo+bestaudio"
         ]
 
-        # Executando o comando
         subprocess.run(command, check=True)
         print("Vídeo baixado com sucesso!")
     except subprocess.CalledProcessError as e:
@@ -24,11 +22,11 @@ def download_video(youtube_url, download_path):
 
 
 if __name__ == "__main__":
-    # Caminho fixo de download
-    download_path = r"C:\Diego\Scripts\youTubeInstaller\Videos"
 
-    # Solicitar o link do YouTube ao usuário
+    path = input("Digite o caminho para a pasta que vai receber o video: ")
+
+    download_path = os.path.join(path, "")
+
     youtube_url = input("Insira o link do vídeo do YouTube: ").strip()
 
-    # Fazer o download do vídeo
     download_video(youtube_url, download_path)
